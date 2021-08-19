4b5e6a9cb8ce498f7870359c2dbf4a4d4fda8739
DmitriyIvakh <ivakh93@ukr.net>
2021-08-20 00:27:51 +0300

    hw22

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable */
 class List {
     list = [];
-    i = 1000;
     constructor(name) {
         this.name = name;
     }
@@ -9,12 +8,8 @@ class List {
     // ======= CREATE ITEM =======
 
     createItem(item) {
-        if (typeof item === 'object') {
-            this.list.push(item);
-            item.id = this.i++;
-        } else {
-            this.list.push({ text: item, id: this.i++ });
-        }
+        this.list.push(item);
+        item.id = Date.now();
     }
 
     // ======= DELETE =======
@@ -23,19 +18,15 @@ class List {
         const deleteCount = 1;
         const index = this.searchedIndex(id);
         this.list.splice(index, deleteCount);
+        if (index === -1) throw new Error('Nothing to delete');
         return this.list;
-        // добавь проверку на -1 => нечего удалять
     }
 
     // ======= EDIT ITEM =======
 
     editItem(id, editedItem) {
         const index = this.searchedIndex(id);
-        if (typeof editedItem === 'object') {
-            this.list[index] = { ...this.list[index], ...editedItem };
-        } else {
-            this.list[index].text = editedItem;
-        }
+        this.list[index] = { ...this.list[index], ...editedItem };
         return this.list;
     }
 
@@ -47,29 +38,25 @@ class List {
 }
 
 // const ololo = new List();
-// ololo.createItem('one');
-// ololo.createItem('two');
-// ololo.createItem('three');
-// // ololo.deleteItem('1001');
-// ololo.editItem('1001', 'five');
+// ololo.createItem({ text: 'one' });
+// ololo.createItem({ text: 'two' });
+// ololo.createItem({ text: 'three' });
+// // // ololo.deleteItem('1001');
+// // // ololo.editItem('1001', { text: 'five' });
 // console.log(ololo.list);
 
 class ToDoList extends List {
     // ======== CREATE TODO ITEM =======
 
-    createItem(text, id = this.i++) {
-        const newItem = {
-            text,
-            id,
-            status: false,
-        };
-        super.createItem(newItem);
+    createItem(item) {
+        item.status = false;
+        super.createItem(item);
     }
 
     // ======= CHANGING A STATUS =======
 
-    changeStatus(item) {
-        const ItemToComplete = this.list[super.searchedIndex(item)];
+    changeStatus(id) {
+        const ItemToComplete = this.list[super.searchedIndex(id)];
         if (!ItemToComplete.status) {
             ItemToComplete.status = true;
         } else {
@@ -87,43 +74,34 @@ class ToDoList extends List {
 }
 
 // const ololo = new ToDoList();
-// ololo.createItem('Сделать 22-ое домашнее задание');
-// ololo.createItem('Завтракать надо');
-// ololo.createItem('Пойти на работу');
-// ololo.createItem('Отдохнуть');
+// ololo.createItem({ text: 'Сделать 22-ое домашнее задание' });
+// ololo.createItem({ text: 'Завтракать надо' });
+// ololo.createItem({ text: 'Пойти на работу' });
+// ololo.createItem({ text: 'Отдохнуть' });
+// ololo.editItem('1002', { text: 'Покакац' });
+// ololo.changeStatus('1002');
 // console.log(ololo.getStatistics());
-// ololo.editItem('1002', 'Покакац');
 // console.log(ololo.list);
 
 class ContactList extends List {
-    // createItem(name, surname, phoneNum) {
-    //     const newItem = {
-    //         name,
-    //         surname,
-    //         phoneNum,
-    //         id: this.i++,
-    //     };
-    //     super.createItem(newItem);
-    // }
-
     searchItem(data) {
         const lowerCaseData = data.toString().toLowerCase();
         return this.list.filter(
             el =>
                 el.name.toLowerCase().includes(lowerCaseData) ||
                 el.surname.toLowerCase().includes(lowerCaseData) ||
-                el.phoneNum.toString().includes(lowerCaseData) ||
+                el.phone.toString().includes(lowerCaseData) ||
                 el.id.toString().includes(lowerCaseData)
         );
     }
 }
 
-const phone = new ContactList();
-phone.createItem({ name: 'Vasya', surname: 'Vasechkin', phone: 380632234513 });
-
-// phone.createItem('Marty', 'McFly', 432435345);
-// phone.createItem('Vasya', 'Pupkin', 58848488);
-// phone.createItem('Emmeth', 'Brown', 70431105);
-// phone.deleteItem('1001');
-console.log(phone.list);
+// const phone = new ContactList();
+// phone.createItem({ name: 'Marty', surname: 'McFly', phone: 58848488 });
+// phone.createItem({ name: 'Vasya', surname: 'Vasechkin', phone: 380632234513 });
+// phone.createItem({ name: 'Emmeth', surname: 'Brown', phone: 70431105 });
+// phone.createItem({ name: 'Maria', surname: 'Grande', phone: 49849848494 });
+// // phone.editItem(1001, { gender: 'unknown' });
+// // phone.deleteItem('1001');
+// console.log(phone.list);
 // // console.log(phone.searchItem('mar'));
